6197f6abbf4cfa5de93b9e856fa825e758e74185
Arkidillo <arkidillo@gmai.com>
2017-02-11 16:45:25 -0500

    Input detection working. TODO: Draw X or O 's

--- src/main/java/com/tictactoebot/UI/GameStateHandler.java
+++ src/main/java/com/tictactoebot/UI/GameStateHandler.java
@@ -8,7 +8,7 @@ import static com.tictactoebot.UI.Frame.*;
  * Created by Devin on 2/11/2017.
  */
 public class GameStateHandler {
-    public static boolean playerTurn;
+    public static boolean playerTurn = true;
     public static char[] boardState = new char[9];
 
     private GameStateHandler(){
@@ -24,13 +24,32 @@ public class GameStateHandler {
 
         if (x < Frame.cornerCoords[0].x && y < Frame.cornerCoords[0].y){
             //Top left
+            System.out.println("Top Left");
         } else if (x < Frame.cornerCoords[1].x && y < Frame.cornerCoords[0].y){
             //Top middle
+            System.out.println("Top Middle");
         } else if (x > Frame.cornerCoords[1].x && y < Frame.cornerCoords[0].y){
             //Top Right
+            System.out.println("Top Right");
         } else if (x < Frame.cornerCoords[2].x && y < Frame.cornerCoords[2].y) {
+            //Middle left
+            System.out.println("Middle Left");
+        } else if (x < Frame.cornerCoords[3].x && y < Frame.cornerCoords[3].y){
+            //Middle middle
+            System.out.println("Middle middle");
+        } else if (x > Frame.cornerCoords[3].x && y < Frame.cornerCoords[3].y){
             //Middle right
-        } //else if (x < Frame.)
+            System.out.println("Middle Right");
+        } else if (x < Frame.cornerCoords[2].x && y > Frame.cornerCoords[3].y){
+            //Bottom Left
+            System.out.println("Bottom Left");
+        } else if (x < Frame.cornerCoords[3].x && y > Frame.cornerCoords[3].y){
+            //Bottom Middle   
+            System.out.println("Bottom Middle"); 
+        } else {
+            //Bottom Right
+            System.out.println("Bottom Right");
+        }
      }
 
 }
