b836f18e3a3dc777f633b1a1e76d01d74ed44c4e
AaronCoplan <afcoplan@gwu.edu>
2017-02-15 12:54:21 -0500

    Refactored main and added 'help' option to get usage information.

--- src/main/java/com/tictactoebot/Main.java
+++ src/main/java/com/tictactoebot/Main.java
@@ -8,9 +8,26 @@ public class Main {
 
     public static void main(String[] args){
 
+        Options options = getOptions(args);
+
+        GameEngine gameEngine = new GameEngine(options);
+        gameEngine.run();
+    }
+
+    public static Options getOptions(String[] args){
+
         boolean randomTrainer = false; // defaults to false
         int numTrainingGames = -1;
 
+        // prints usage information
+        if(args.length == 1 && args[0].equals("help")){
+            System.out.println("Usage information:");
+            System.out.println("To play against the computer: java -jar build/libs/tictactoebot-v0.0.1.jar");
+            System.out.println("To train the bot: java -jar build/libs/tictactoebot-v0.0.1.jar train <num games>");
+            System.exit(0);
+        }
+
+        // activates trainimg mode
         if(args.length == 2 && args[0].equals("train")){
             randomTrainer = true;
             numTrainingGames = Integer.parseInt(args[1]);
@@ -23,7 +40,6 @@ public class Main {
         }
         options.setMillisBetweenGames(4000); // 4 second delay between games
 
-        GameEngine gameEngine = new GameEngine(options);
-        gameEngine.run();
+        return options;
     }
 }
